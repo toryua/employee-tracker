1bb3b5582a99b651a6f195aad4fe1e20ab1548f2
toryua <87789457+toryua@users.noreply.github.com>
2021-10-22 12:25:21 -0700

    added values to seed file

---
+++ db/seeds/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (department_id, department_name)
+VALUES  (1, "Sales"),
+        (2, "Human Resources"),
+        (3, "Information Technology");
+
+INSERT INTO role (role_id, title)
+VALUES  (1, "Engineer"),
+        (2, "Salesperson"),
+        (3, "Developer"),
+        (4, "Human Resources Representative"),
+        (5, "Manager");
+
+INSERT INTO employees (first_name, last_name)
+VALUES  ("Charmaine", "Walton"),
+        ("Katharine", "Welsh"),
+        ("Francis", "Randall"),
+        ("Amelia-Rose", "Thorne"),
+        ("Ruqayyah", "Irving"),
+        ("Bronwyn", "Hirst"),
+        ("Frederick", "Hall"),
+        ("Daniel", "Atkins"),
+        ("Zaid", "Rigley"),
+        ("Laurel", "Gutierrez");
